08e29596b6a4ae5be85aa8580ae9c8e5b88e470e
Rakshitha S <142673224+Rakshitha030@users.noreply.github.com>
2025-08-09 01:18:12 +0530

    Add files via upload

---
+++ pll_patterns.py
@@ -0,0 +1,29 @@
+PLL_PATTERNS = {
+    'PLL_Ua': (['WGR', 'WRB', 'WBO', 'WOG'], ['WR', 'WB', 'WO', 'WG']),
+    'PLL_Ub': (['WGR', 'WRB', 'WBO', 'WOG'], ['WG', 'WR', 'WB', 'WO']),
+    'PLL_Z':  (['WGR', 'WRB', 'WBO', 'WOG'], ['WR', 'WO', 'WG', 'WB']),
+    'PLL_H':  (['WGR', 'WRB', 'WBO', 'WOG'], ['WO', 'WR', 'WG', 'WB']),
+    
+    'PLL_Aa': (['WRB', 'WBO', 'WOG', 'WGR'], ['WR', 'WG', 'WO', 'WB']),
+    'PLL_Ab': (['WBO', 'WOG', 'WGR', 'WRB'], ['WB', 'WO', 'WR', 'WG']),
+    
+    'PLL_E':  (['WGR', 'WRB', 'WBO', 'WOG'], ['WB', 'WG', 'WR', 'WO']),
+    
+    'PLL_F':  (['WRB', 'WGR', 'WOG', 'WBO'], ['WG', 'WR', 'WO', 'WB']),
+    'PLL_V':  (['WOG', 'WBO', 'WRB', 'WGR'], ['WO', 'WB', 'WR', 'WG']),
+    'PLL_Y':  (['WGR', 'WOG', 'WBO', 'WRB'], ['WR', 'WO', 'WB', 'WG']),
+    'PLL_Na': (['WOG', 'WRB', 'WBO', 'WGR'], ['WO', 'WR', 'WB', 'WG']),
+    'PLL_Nb': (['WRB', 'WBO', 'WGR', 'WOG'], ['WR', 'WB', 'WG', 'WO']),
+    
+    'PLL_Ra': (['WBO', 'WGR', 'WOG', 'WRB'], ['WB', 'WR', 'WO', 'WG']),
+    'PLL_Rb': (['WRB', 'WBO', 'WGR', 'WOG'], ['WR', 'WB', 'WG', 'WO']),
+    
+    'PLL_Ja': (['WOG', 'WGR', 'WRB', 'WBO'], ['WO', 'WR', 'WB', 'WG']),
+    'PLL_Jb': (['WRB', 'WBO', 'WOG', 'WGR'], ['WR', 'WB', 'WO', 'WG']),
+    
+    'PLL_T':  (['WGR', 'WRB', 'WOG', 'WBO'], ['WR', 'WB', 'WO', 'WG']),
+    'PLL_Ga': (['WGR', 'WBO', 'WRB', 'WOG'], ['WR', 'WB', 'WO', 'WG']),
+    'PLL_Gb': (['WRB', 'WOG', 'WGR', 'WBO'], ['WR', 'WB', 'WO', 'WG']),
+    'PLL_Gc': (['WBO', 'WRB', 'WOG', 'WGR'], ['WR', 'WB', 'WO', 'WG']),
+    'PLL_Gd': (['WOG', 'WGR', 'WBO', 'WRB'], ['WR', 'WB', 'WO', 'WG']),
+}
